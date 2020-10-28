2ed888ffdb684c896390e1a9ef53c2801e8ee4d4
george <reguralnetuser@gmail.com>
2020-10-28 14:40:11 +0300

    Drawing simple rectangle

--- src/test.cpp
+++ src/test.cpp
@@ -36,9 +36,15 @@ int main(void)
     glewInit();
 
     float positions[] = {
+         0.5,  0.5, 0.0,
+         0.5, -0.5, 0.0,
         -0.5, -0.5, 0.0,
-         0.5, -0.5, 0.0, 
-         0.0,  0.5, 0.0
+        -0.5,  0.5, 0.0 
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
     };
 
     unsigned int vao;
@@ -50,6 +56,11 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
     glEnableVertexAttribArray(0);
@@ -60,13 +71,12 @@ int main(void)
     unsigned int shader = CreateShader(shaderSrc.vertexShader, shaderSrc.fragmentShader);
 
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -74,6 +84,7 @@ int main(void)
 
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
     glDeleteProgram(shader);
     glfwTerminate();
     return 0;
